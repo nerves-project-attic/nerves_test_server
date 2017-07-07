b0755367c34c0aa4c8d95308804762cea287dc9f
Justin Schneck <jschneck@mac.com>
2017-07-07 13:11:57 -0400

    remove database name from the prod config

--- config/prod.exs
+++ config/prod.exs
@@ -27,7 +27,6 @@ config :nerves_test_server, NervesTestServer.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  database: "nerves_test_server_prod",
   ssl: true
 
 # ## SSL Support
